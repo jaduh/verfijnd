b6f340f6a5c505bedb33e9ccb42dcab99dd7fc7c
jaduh <jadeyorks@outlook.com>
2016-07-20 16:38:42 +0200

    initial commit of files

---
+++ JS/verfijnd2.js
@@ -0,0 +1,9 @@
+/*var h = window.innerHeight;
+function height() {
+	document.getElementById('Pagina').style.height = h+"px";
+	document.getElementById('Pagina1').style.height = h+"px";
+	document.getElementById('Pagina2').style.height = h+"px";
+	document.getElementById('Pagina3').style.height = h+"px";
+
+	window.addEventListener('load',height);
+} 
